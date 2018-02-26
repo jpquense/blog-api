2f99a2a6372ba762f2b09e6ee073030e8f49f368
John P Quense <jpquense@gmail.com>
2018-02-25 21:04:07 -0500

    changed test file in an attempt to make the npm test run with mocha and chia

--- test/test-server.js
+++ test/test-server.js
@@ -3,8 +3,8 @@ const chaiHttp = require('chai-http');
 
 const {app, runServer, closeServer} = require('../server');
 const expect = chai.expect;
-
 chai.use(chaiHttp);
+
 
 describe('BlogPosts', function() {
     before(function() {
