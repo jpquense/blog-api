2e3ff7e75248419041dde04bd8e59b40a91b4a12
John P Quense <jpquense@gmail.com>
2018-02-25 20:46:05 -0500

    added test file for blog router

--- server.js
+++ server.js
@@ -1,16 +1,45 @@
 const express = require('express');
 const morgan = require('morgan');
-
-const blogPostsRouter = require('./blogPostsRouter');
 const app = express();
-
+const blogPostsRouter = require('./blogPostsRouter');
 
 app.use(morgan('common'));
-
-// you need to import `blogPostsRouter` router and route
-// requests to HTTP requests to `/blog-posts` to `blogPostsRouter`
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+ 
 app.use('/blog-posts', blogPostsRouter);
 
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
-});
+let server;
+
+function runServer() {
+  const port = process.env.PORT || 8080;
+  return new Promise((resolve, reject) => {
+    server = app.listen(port, () => {
+      console.log(`Your app is listening on port ${port}`);
+      resolve(server);
+    }).on('error', err => {
+      reject(err)
+    });
+  });
+}
+
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log('Closing server');
+    server.close(err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
+
+module.exports = {app, runServer, closeServer};
---
+++ test/test-server.js
@@ -0,0 +1,81 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const {app, runServer, closeServer} = require('../server');
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('BlogPosts', function() {
+    before(function() {
+        return runServer();
+    });
+    after(function() {
+        return closeServer();
+    });
+
+    it('should list posts on GET', function() {
+        return chai.request(app)
+            .get('/blog-posts')
+            .then(function(res) {
+                expect(res).to.have.status(200);
+                expect(res).to.be.json;
+                expect(res.body).to.be.a('array');
+                const expectedKeys = ['id', 'title', 'content', 'author'];
+                res.body.forEach(function(item) {
+                    expect(item).to.be.a('object');
+                    expect(item).to.include.keys(expectedKeys);
+                });
+            });
+    });
+
+    it('should add an post on POST', function() {
+        const newItem = {title: 'coffee', content: 'Hello I am a fun guy blah blah blah', author: 'John Q', publishDate: 'March 30, 2018'};
+        return chai.request(app)
+          .post('/blog-posts')
+          .send(newItem)
+          .then(function(res) {
+            expect(res).to.have.status(201);
+            expect(res).to.be.json;
+            expect(res.body).to.be.a('object');
+            expect(res.body).to.include.keys('id', 'title', 'content', 'author');
+            expect(res.body.id).to.not.equal(null);
+            expect(res.body).to.deep.equal(Object.assign(newItem, {id: res.body.id}));
+          });
+      });
+    
+      it('should update items on PUT', function() {
+        const updateData = {
+            title: 'foo',
+            content: 'Hello i am john the wonderful',
+            author: 'John Q'
+        };
+    
+        return chai.request(app)
+          .get('/blog-posts')
+          .then(function(res) {
+            updateData.id = res.body[0].id;
+            return chai.request(app)
+              .put(`/blog-posts/${updateData.id}`)
+              .send(updateData);
+          })
+          .then(function(res) {
+            expect(res).to.have.status(200);
+            expect(res).to.be.json;
+            expect(res.body).to.be.a('object');
+            expect(res.body).to.deep.equal(updateData);
+          });
+      });
+
+      it('should delete items on DELETE', function() {
+        return chai.request(app)
+          .get('/blog-posts')
+          .then(function(res) {
+            return chai.request(app)
+              .delete(`/blog-posts/${res.body[0].id}`);
+          })
+          .then(function(res) {
+            expect(res).to.have.status(204);
+          });
+      });
+});
